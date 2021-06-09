6799baab0fb60a0a1fdb4233873920963db716eb
Beny Allan <benyallan@gmail.com>
2021-06-09 00:39:47 -0300

    criado pagina principal simples

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,6 @@
 @extends('layouts.principal')
 
 @section('principal')
-<div>
-    <h1>Página principal</h1>
-</div>
+<h1>Template Bootstrap inicial</h1>
+          <p class="lead">Use este documento como uma maneira de iniciar um novo projeto, rapidamente.<br>Tudo oquê você começa é com este texto e um documento HTML (quase vazio).</p>
 @endsection
