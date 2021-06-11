7df38df2784cfb82c0e16e3944cdebe72c10564b
Beny Allan <benyallan@gmail.com>
2021-06-11 20:30:26 -0300

    pequena traduçao nos dashs

--- resources/views/administrador/dashboard.blade.php
+++ resources/views/administrador/dashboard.blade.php
@@ -1,11 +1,5 @@
 @extends('adminlte::page')
 
-@section('header')
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-        {{ __('Dashboard') }}
-    </h2>
-@endsection
-
 @section('content')
     <div>
         Você está logado como administrador
