9d5cb4cee5b2cddd15151f96744867df6a0e0718
Beny Allan <benyallan@gmail.com>
2021-06-16 18:01:31 -0300

    pagina de ediçao de função em andamento

--- resources/views/administrador/funcionarios/roles/editar.blade.php
+++ resources/views/administrador/funcionarios/roles/editar.blade.php
@@ -1,6 +1,40 @@
 @extends('adminlte::page')
 
 @section('content')
-{{-- Com vários slots, parâmetro de configuração do plug-in e opções personalizadas --}}
+
+<form action="" method="post">
+    {{-- With prepend slot --}}
+    <x-adminlte-input name="role" label="Função" placeholder="{{$role->name}}" label-class="text-lightblue">
+        <x-slot name="prependSlot">
+            <div class="input-group-text">
+                <i class="fas fa-briefcase text-lightblue"></i>
+            </div>
+        </x-slot>
+    </x-adminlte-input>
+
+    <select data-actions-box="true" class="selectpicker" data-width="100%" multiple title="Escolha as permissões..." data-style="btn-info">
+        <optgroup label="Funções">
+          @foreach ($permission as $key)
+              @if (Str::contains($key->name, 'funções') && !Str::contains($key->name, 'funcionários'))
+                <option>{{$key->name}}</option>
+              @endif
+          @endforeach
+        </optgroup>
+        <optgroup label="Funcionários">
+            @foreach ($permission as $key)
+              @if (Str::contains($key->name, 'funcionários'))
+                <option>{{$key->name}}</option>
+              @endif
+          @endforeach
+        </optgroup>
+        <optgroup label="Clientes">
+            @foreach ($permission as $key)
+              @if (Str::contains($key->name, 'clientes'))
+                <option>{{$key->name}}</option>
+              @endif
+          @endforeach
+          </optgroup>
+      </select>
+</form>
 
 @endsection
